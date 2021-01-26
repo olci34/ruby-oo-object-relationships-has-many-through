5b217c6342ab7fcdee376ebc2e1a82714f804ac7
github username <github email address>
2021-01-26 17:50:19 -0500

    commit tree 925046ce09507c421f83d3cb2abc98f405195507

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,16 @@
 class Meal
+    attr_accessor :customer, :waiter, :total, :tip
+    @@all = []
+    def initialize(waiter, customer, total, tip = 0)
+        self.waiter = waiter
+        self.customer = customer
+        self.total = total
+        self.tip = tip
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+    
 end
